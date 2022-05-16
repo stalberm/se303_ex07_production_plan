c8c7016fc45e6292fafd80b55bb90b7ff7810b68
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-15 18:53:12 -0700

    Add unit test for empty string demand

--- test/province_test.rb
+++ test/province_test.rb
@@ -47,4 +47,10 @@ class ProvinceTest < Minitest::Test
     assert_equal(-26, asia.shortfall)
     assert_equal(-10, asia.profit)
   end
+
+  def test_empty_string_demand
+    asia.demand = ""
+    assert_raises(NoMethodError) {asia.shortfall}
+    assert_raises(ArgumentError) {asia.profit}
+  end
 end
