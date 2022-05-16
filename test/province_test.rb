421f08cb6a7c493ed9b9100ee2a6d9787a6870b4
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-15 17:48:31 -0700

    Add unit test for change of province production

--- test/province_test.rb
+++ test/province_test.rb
@@ -17,4 +17,10 @@ class ProvinceTest < Minitest::Test
   def test_province_profit
     assert_equal(230, asia.profit)
   end
+
+  def test_change_production
+    asia.producers[0].production = 20
+    assert_equal(-6, asia.shortfall)
+    assert_equal(292, asia.profit)
+  end
 end
